9cdcced7d9f28d1e2876d8964e0f967965ec9c41
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-13 05:36:09 +0100

    Add ability to exit program by pressing escape.

--- ball-in-maze/main.cpp
+++ ball-in-maze/main.cpp
@@ -2,22 +2,30 @@
 #include "pandaSystem.h"
 
 //Some constants for the program
-int ACCEL = 70;									//Acceleration in ft/sec/sec
+int ACCELERATION = 70;							//Acceleration in ft/sec/sec
 int MAX_SPEED = 5;								//Max speed in ft/sec
 int MAX_SPEED_SQ = MAX_SPEED * MAX_SPEED;		//MAX_SPEED squared
 
+//Exit function
+void panda_exit(const Event* theEvent, void* data) {
+	exit(0);
+}
+
 int main(int argc, char* argv[]) {
-	// Open a new window framework
+
+	//Open framework
 	PandaFramework framework;
 	framework.open_framework(argc, argv);
 
-	// Set the window title and open the window
-	framework.set_window_title("My Panda3D Window");
+	//Set the window title and open new window
+	framework.set_window_title("Ball in maze - C++ Panda3D Samples");
 	WindowFramework* window = framework.open_window();
 
-	// Here is room for your own code
+	//Keyboard definitions
+	window->enable_keyboard();
+	framework.define_key("escape", "Escape-key", panda_exit, 0);
 
-	// Do the main loop, equal to run() in python
+	//Do the main loop
 	framework.main_loop();
 	framework.close_framework();
 	return 0;
