IsakTheHacker/c-p3d-samples

commit 01841aa909ecb97272d84537431ba0a64f467a43

Removed CharAnimate everywhere but boxing-robots boxing-robots is what I originally wrote it for, but it's better to use AnimControl directly if using asynchronously.  Unless I missed something, though, it's not possible to await the end of the animation, unless you compute the time yourself and assume it's done by that time.  I will not rewrite CharAnimate to do that, though. Since the only remaining AnimControl support isn't dependent on intervals, I moved it to its own file.  That way, you don't have to link to p3direct just to more easily load animations.

looking-and-gripping/main.cpp
@@ -113,7 +113,7 @@ void init(void)
     window->get_camera_group().set_pos(0, -15, 2);  // Position the camera
 
     auto eve = def_load_model("models/eve");  // Load our animated charachter
-    auto walk = load_anim(eve, sample_path + "models/eve_walk");
+    PT(AnimControl) walk = load_anim(eve, sample_path + "models/eve_walk");
     eve.reparent_to(window->get_render());  // Put it in the scene
 
     // Now we use control_joint() to control her neck
@@ -137,7 +137,7 @@ void init(void)
     // We now play an animation. An animation must be played, or at least posed
     // for the nodepath we just got from control_joint to actually affect the
     // model
-    (new CharAnimate(walk, 2))->loop();
+    walk->loop(true);
     // Now we add a task that will take care of turning the head
     framework.get_task_mgr().add(new GenericAsyncTask("turn_head", turn_head,
 						      (void*)window->get_mouse().node()));

boxing-robots/main.cpp
@@ -52,6 +52,53 @@ void gen_label_text(const char *text, int i)
     text_node->set_align(TextNode::A_left);
 }
 
+// AnimControl is asynchronous, so make it more Interval-like by stepping
+// through it one frame (pose) at a time in a custom CInterval:
+class CharAnimate : public CInterval {
+  public:
+    // constructor
+    CharAnimate(AnimControl *ctrl, double rate = 1.0,
+		double start = 0, double end = -1) :
+	// intervals need a globally unique name, so why not its address?
+	CInterval(std::to_string((unsigned long)this), 0, false) {
+	    _ctrl = ctrl;
+	    auto bundle = ctrl->get_anim();
+	    if(end > bundle->get_num_frames() || end < 0)
+		end = bundle->get_num_frames() - 1;
+	    if(start < 0)
+		start = 0;
+	    _end_t = _duration = (end - start + 1) / rate / bundle->get_base_frame_rate();
+	    set_play_rate(rate);
+	    _start = start;
+	    _end = end;
+	}
+    // "private" interval step override
+    // just compute the current frame and pose it.
+    void priv_step(double t)
+    {
+	double frame = _start + (_end - _start) / (_duration ? _duration : 1) * t;
+	_ctrl->pose(frame);
+    }
+    // "private" interval startup override
+    // cause the animation to influence parts
+    void priv_initialize(double t) {
+	_ctrl->get_part()->set_control_effect(_ctrl, 1);
+	CInterval::priv_initialize(t);
+    }
+    // "private" interval shutdown override
+    // cause the animation to stop influencing parts
+    void priv_finalize() {
+	_ctrl->get_part()->set_control_effect(_ctrl, 0);
+	CInterval::priv_finalize();
+    }
+    // NOTE: adding members, like below, requires this:
+    ALLOC_DELETED_CHAIN(CharAnimate);
+  protected:
+    PT(AnimControl) _ctrl;
+    int _start, _end;
+};
+
+
 void try_punch(const Event *, void *);
 void check_punch(int);
 void setup_lights(void);
@@ -131,13 +178,13 @@ void init()
     robot[1].reparent_to(window->get_render());
 
     // Now we define how the animated models will move. Animations are played
-    // through special intervals.  Since the Python code uses special convenience
-    // wrappers, this code does, as well.  They are defined in the common
-    // anim_supt.h file.  In this case we use a custom CInterval (CharAnimate,
-    // located in anim_supt.h) that runs the animation directly.  This
-    // is used in a sequence to play the part of the punch animation where the
-    // arm extends, call a function to check if the punch landed, and then
-    // play the part of the animation where the arm retracts
+    // via their AnimControl.  However, this is entirely asynchronous, and we
+    // need to do things when the animation is done.  Thus, we use a special
+    // custom CInterval (CharAnimate) that steps through the animation via
+    // its control.  It is defined below. This is used in a Sequence to
+    // play the part of the punch animation where the arm extends, call a
+    // function to check if the punch landed, and then play the part of
+    // the animation where the arm retracts
 
     for(int r = 0; r < 2; r++) {
 	// Punch sequence for robot's left arm

render-to-texture/main.cpp
@@ -27,7 +27,7 @@ std::string sample_path
 #endif
 	;
 NodePath alt_cam;
-std::vector<PT(CInterval)> tv_men;
+std::vector<PT(AnimControl)> tv_men;
 
 // Function to put instructions on the screen.
 void add_instructions(PN_stdfloat pos, const std::string &msg)
@@ -165,8 +165,9 @@ void make_tv_man(PN_stdfloat x, PN_stdfloat y, PN_stdfloat z, Texture *tex,
     auto anim_node = DCAST(AnimBundleNode, man.get_child(1).node());
     auto anim = character->get_bundle(0)->bind_anim(anim_node->get_bundle(), ANIM_BIND_FLAGS);
     anim->set_anim_model(anim_node);
-    tv_men.push_back(new CharAnimate(anim, playrate));
-    tv_men.back()->loop();
+    anim->set_play_rate(playrate);
+    tv_men.push_back(anim);
+    tv_men.back()->loop(true);
 }
 
 void zoom_in()

roaming-ralph/main.cpp
@@ -33,8 +33,7 @@ std::string sample_path
 #endif
     ;
 NodePath ralph, floater;
-PT(CInterval) run;
-PT(AnimControl) walk;
+PT(AnimControl) run, walk;
 CollisionTraverser c_trav;
 PT(CollisionHandlerQueue) ralph_ground_handler, cam_ground_handler;
 // Game state variables
@@ -81,7 +80,6 @@ void init(void)
 {
     // Set up the window, camera, etc.
     framework.open_framework();
-    update_intervals();
     framework.set_window_title("Roaming Ralph - C++ Panda3D Samples");
     window = framework.open_window();
 
@@ -117,9 +115,8 @@ void init(void)
     // Create the main character, Ralph
 
     auto ralph_start_pos = environ.find("**/start_point").get_pos();
-    std::cerr << ralph_start_pos << '\n';
     ralph = def_load_model("models/ralph");
-    run = new CharAnimate(load_anim(ralph, sample_path + "models/ralph-run"));
+    run = load_anim(ralph, sample_path + "models/ralph-run");
     walk = load_anim(ralph, sample_path + "models/ralph-walk");
     ralph.reparent_to(render);
     ralph.set_scale(.2);
@@ -238,11 +235,11 @@ AsyncTask::DoneStatus move(GenericAsyncTask *, void *)
 
     if(key_map[k_forward] || key_map[k_left] || key_map[k_right]) {
 	if(!is_moving) {
-	    run->loop();
+	    run->loop(true);
 	    is_moving = true;
         }
     } else if(is_moving) {
-	run->finish();
+	run->stop();
 	walk->pose(5);
 	is_moving = false;
     }
@@ -320,7 +317,6 @@ int main(int argc, char* argv[]) {
     init();
 
     framework.main_loop();
-    kill_intervals();
     framework.close_framework();
     return 0;
 }

cartoon-shader/advanced.cpp
@@ -35,6 +35,7 @@ std::string sample_path
 	;
 PN_stdfloat separation = 0.001, cutoff = 0.3;
 NodePath drawn_scene;
+PT(AnimControl) anim;
 
 // Function to put instructions on the screen.
 void add_instructions(PN_stdfloat pos, const std::string &msg)
@@ -179,9 +180,9 @@ void init(void)
     // Scan for the anim bundlue (should be 2nd child)
     //auto anim_node = DCAST(AnimBundleNode, char_model.find("**/+AnimBundleNode").node());
     auto anim_node = DCAST(AnimBundleNode, char_model.get_child(1).node());
-    auto anim = character->get_bundle(0)->bind_anim(anim_node->get_bundle(), ANIM_BIND_FLAGS);
+    anim = character->get_bundle(0)->bind_anim(anim_node->get_bundle(), ANIM_BIND_FLAGS);
     anim->set_anim_model(anim_node);
-    (new CharAnimate(anim))->loop();
+    anim->loop(true);
     auto spin = new NPAnim(char_model, "spin", 15);
     spin->set_end_hpr({360, 0, 0});
     spin->loop();

glow-filter/advanced.cpp
@@ -30,8 +30,7 @@ std::string sample_path
 #endif
 	;
 NodePath tron, finalcard;
-PT(CInterval) running;
-PT(AnimControl) running_ctl;
+PT(AnimControl) running;
 PT(NPAnim) interval;
 bool is_running, glow_on;
 GraphicsOutput *glow_buffer;
@@ -141,8 +140,7 @@ void init(void)
 
     // load our model
     tron = def_load_model("models/tron");
-    running_ctl = load_anim(tron, sample_path + "models/tron_anim");
-    running = new CharAnimate(running_ctl);
+    running = load_anim(tron, sample_path + "models/tron_anim");
     auto render = window->get_render();
     tron.reparent_to(render);
     interval = new NPAnim(tron, "interval", 60);
@@ -228,14 +226,14 @@ void toggle_display()
     auto camera = window->get_camera_group();
     if(!is_running) {
 	camera.set_pos(0, -50, 0);
-	running->finish();
-	running_ctl->pose(0);
+	running->stop();
+	running->pose(0);
 	interval->loop();
     } else {
 	camera.set_pos(0, -170, 3);
 	interval->finish();
 	tron.set_hpr(0, 0, 0);
-	running->loop();
+	running->loop(true);
     }
 }
 }

looking-and-gripping/CMakeLists.txt
@@ -14,5 +14,5 @@ include(../supt/samples.cmake)
 
 #### Build executable
 add_executable(${SAMPLE} main.cpp)
-target_link_libraries(${SAMPLE} ${PANDALIBS} ${PANDA-p3direct} sample_supt)
+target_link_libraries(${SAMPLE} ${PANDALIBS} sample_supt ${PANDA-p3direct})
 install(TARGETS ${SAMPLE})

motion-trails/CMakeLists.txt
@@ -15,7 +15,6 @@ include(../supt/samples.cmake)
 #### Build executables
 foreach(x framebuffer-feedback) # fireball) # fireball is too Python-heavy
   add_executable(${x} ${x}.cpp)
-  target_link_libraries(${x} ${PANDALIBS} ${PANDA-p3direct} sample_supt)
+  target_link_libraries(${x} ${PANDALIBS} sample_supt)
   install(TARGETS ${x})
 endforeach()
-

commit b661d091f65135b98b314de4333660f979b345ee

Fixed looking-and-gripping broken in 01841aa909ecb97272d84537431ba0a64f467a43   - create non-local ref for walk (otherwise no play)   - adjust play speed to 2.0

looking-and-gripping/main.cpp
@@ -35,6 +35,7 @@ namespace { // don't export/pollute the global namespace
 // they need to all be delcared at the top.
 PandaFramework framework;
 WindowFramework *window;
+PT(AnimControl) walk;
 NodePath eve_neck, right_hand,
     models[4]; // A list that will store our models objects
 PT(CInterval) anim;
@@ -68,9 +69,6 @@ void setup_lights(void);
 
 void init(void)
 {
-    Notify::ptr()->get_category(":loader")->set_severity(NS_spam);
-    Notify::ptr()->get_category(":egg")->set_severity(NS_spam);
-    
     // Open framework (replaces ShowBase).  This will not set up
     // everything like ShowBase, but it does provide convenient functions
     // to do so.
@@ -111,7 +109,7 @@ void init(void)
     window->get_camera_group().set_pos(0, -15, 2);  // Position the camera
 
     auto eve = def_load_model("models/eve");  // Load our animated charachter
-    PT(AnimControl) walk = load_anim(eve, "models/eve_walk");
+    walk = load_anim(eve, "models/eve_walk");
     eve.reparent_to(window->get_render());  // Put it in the scene
 
     // Now we use control_joint() to control her neck
@@ -135,6 +133,7 @@ void init(void)
     // We now play an animation. An animation must be played, or at least posed
     // for the nodepath we just got from control_joint to actually affect the
     // model
+    walk->set_play_rate(2.0);
     walk->loop(true);
     // Now we add a task that will take care of turning the head
     framework.get_task_mgr().add(new GenericAsyncTask("turn_head", turn_head,